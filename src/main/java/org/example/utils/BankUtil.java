1efdf2d1e0d6161b85c546d60e61d2218d828b77
Orxan745 <orxan8586orxan@gmail.com>
2025-02-15 09:29:43 +0400

    commit

--- src/main/java/org/example/utils/BankUtil.java
+++ src/main/java/org/example/utils/BankUtil.java
@@ -4,7 +4,6 @@ import org.example.model.Customer;
 import org.example.model.Transaction;
 import java.io.EOFException;
 import java.io.IOException;
-import java.util.Objects;
 import static org.example.model.Bank.*;
 import static org.example.constants.FileConstants.*;
 import static org.example.utils.FileUtils.readObjectFromFile;
@@ -26,7 +25,7 @@ public class BankUtil {
     public static void initializeCustomerData() {
         try {
             Object object = readObjectFromFile(CUSTOMER_FILE_NAME);
-            if (Objects.nonNull(object)) {
+            if (object != null) {
                 customers = (Customer[]) object;
             }
         } catch (EOFException e) {
@@ -35,7 +34,7 @@ public class BankUtil {
 
         int length = 0;
         for (Customer customer : customers) {
-            if (Objects.nonNull(customer)) {
+            if (customer != null) {
                 length++;
             }
         }
@@ -45,7 +44,7 @@ public class BankUtil {
     public static void initializeTransactionData() {
         try {
             Object object = readObjectFromFile(TRANSACTION_FILE_NAME);
-            if (Objects.nonNull(object)) {
+            if (object != null) {
                 transactions = (Transaction[]) object;
             }
         } catch (EOFException e) {
@@ -54,7 +53,7 @@ public class BankUtil {
 
         int length = 0;
         for (Transaction transaction : transactions) {
-            if (Objects.nonNull(transaction)) {
+            if (transaction != null) {
                 length++;
             }
         }
